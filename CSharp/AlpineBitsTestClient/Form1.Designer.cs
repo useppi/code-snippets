f2cc9cc9c2448002d5476b0590cad3b2d2ae1ed7
Ulrich Seppi <ulrich.seppi@lts.it>
2015-04-28 16:00:56 +0200

    AlpineBits test client as .NET project.
    It requires .NET framework 4

---
+++ CSharp/AlpineBitsTestClient/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace AlpineBitsTestClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRequest = new System.Windows.Forms.RichTextBox();
+            this.txtResponse = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtMessagePWD = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.cBGZIP = new System.Windows.Forms.CheckBox();
+            this.cBGZIPSend = new System.Windows.Forms.CheckBox();
+            this.cbAction = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtRequest
+            // 
+            this.txtRequest.Location = new System.Drawing.Point(13, 181);
+            this.txtRequest.MaxLength = 3276700;
+            this.txtRequest.Name = "txtRequest";
+            this.txtRequest.Size = new System.Drawing.Size(624, 564);
+            this.txtRequest.TabIndex = 6;
+            this.txtRequest.Text = "";
+            this.txtRequest.TextChanged += new System.EventHandler(this.txtRequest_TextChanged);
+            this.txtRequest.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtRequest_KeyDown);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(646, 218);
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(639, 527);
+            this.txtResponse.TabIndex = 10;
+            this.txtResponse.Text = "no result";
+            this.txtResponse.TextChanged += new System.EventHandler(this.txtResponse_TextChanged);
+            this.txtResponse.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtResponse_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Process request to server >>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Action:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(643, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Url to AlpineBits Server Endpoint:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "OTA Request message (RQ)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(643, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "SERVERS OTA response message (RS):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Username";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "X-AlpineBits-ClientID";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(219, 15);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(242, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(219, 41);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(242, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // txtMessagePWD
+            // 
+            this.txtMessagePWD.Location = new System.Drawing.Point(219, 67);
+            this.txtMessagePWD.Name = "txtMessagePWD";
+            this.txtMessagePWD.Size = new System.Drawing.Size(242, 20);
+            this.txtMessagePWD.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(166, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "X-AlpineBits-ClientProtocolVersion";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(219, 94);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(242, 20);
+            this.txtCompanyName.TabIndex = 4;
+            // 
+            // cBGZIP
+            // 
+            this.cBGZIP.AutoSize = true;
+            this.cBGZIP.Checked = true;
+            this.cBGZIP.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cBGZIP.Location = new System.Drawing.Point(646, 63);
+            this.cBGZIP.Name = "cBGZIP";
+            this.cBGZIP.Size = new System.Drawing.Size(235, 17);
+            this.cBGZIP.TabIndex = 8;
+            this.cBGZIP.Text = "Include \"Accept-Encoding: gzip\" in Request";
+            this.cBGZIP.UseVisualStyleBackColor = true;
+            // 
+            // cBGZIPSend
+            // 
+            this.cBGZIPSend.AutoSize = true;
+            this.cBGZIPSend.Checked = true;
+            this.cBGZIPSend.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cBGZIPSend.Location = new System.Drawing.Point(646, 87);
+            this.cBGZIPSend.Name = "cBGZIPSend";
+            this.cBGZIPSend.Size = new System.Drawing.Size(232, 17);
+            this.cBGZIPSend.TabIndex = 9;
+            this.cBGZIPSend.Text = "POST Request Message as GZIP to Server";
+            this.cBGZIPSend.UseVisualStyleBackColor = true;
+            // 
+            // cbAction
+            // 
+            this.cbAction.FormattingEnabled = true;
+            this.cbAction.Items.AddRange(new object[] {
+            "getVersion",
+            "getCapabilities",
+            "OTA_HotelAvailNotif:FreeRooms",
+            "OTA_HotelInvNotif:Inventory",
+            "OTA_Read:GuestRequests",
+            "OTA_NotifReport:GuestRequests",
+            "OTA_HotelRatePlanNotif:RatePlans",
+            "OTA_HotelRatePlanNotif:SimplePackages"});
+            this.cbAction.Location = new System.Drawing.Point(54, 141);
+            this.cbAction.Name = "cbAction";
+            this.cbAction.Size = new System.Drawing.Size(289, 21);
+            this.cbAction.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(362, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 34);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "Validate XSD";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(646, 34);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(622, 20);
+            this.txtServer.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1294, 757);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.cbAction);
+            this.Controls.Add(this.cBGZIPSend);
+            this.Controls.Add(this.cBGZIP);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtMessagePWD);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.txtRequest);
+            this.Name = "Form1";
+            this.Text = "Generic AlpineBits TestClient";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtRequest;
+        private System.Windows.Forms.RichTextBox txtResponse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtMessagePWD;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.CheckBox cBGZIP;
+        private System.Windows.Forms.CheckBox cBGZIPSend;
+        private System.Windows.Forms.ComboBox cbAction;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ CSharp/AlpineBitsTestClient/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO.Compression;
+using System.Xml;
+using System.Configuration;
+using System.Xml.Schema;
+using System.Drawing;
+
+
+namespace AlpineBitsTestClient
+{
+    public partial class Form1 : Form
+    {
+        string sXSDValidationErrorString ="";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResponse.Text = "";
+            button1.Text = "Loading ....";
+            this.Refresh();
+            string password = txtPassword.Text;
+            string url = txtServer.Text;
+            string username = txtUsername.Text;
+          
+
+            // OTA_INVENTORY
+            var content = new MultipartFormDataContent(Guid.NewGuid().ToString());
+            //Note: it's a best practice to use double-quotes, even when the name doesn't contain spaces:
+            content.Add(new StringContent(cbAction.SelectedItem.ToString()), "\"action\"");
+            string RequestXML = txtRequest.Text;
+            if (cbAction.SelectedItem.ToString() != "getVersion" && cbAction.SelectedItem.ToString() != "getCapabilities")
+                content.Add(new StringContent(RequestXML), "\"request\"");
+            HttpResponseMessage result;
+
+            // Client: acceptEncoding gzIP
+            HttpClientHandler clHandler = new HttpClientHandler();
+            if (cBGZIP.Checked)
+                clHandler.AutomaticDecompression = DecompressionMethods.GZip;
+
+
+            using (var client = new HttpClient(clHandler))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes(string.Format("{0}:{1}", username, password))));
+                client.DefaultRequestHeaders.Add("X-AlpineBits-ClientProtocolVersion", txtCompanyName.Text);
+                client.DefaultRequestHeaders.Add("X-AlpineBits-ClientID", txtMessagePWD.Text);
+                if (cBGZIPSend.Checked)
+                    result = client.PostAsync(url, new CompressedContent(content, "gzip")).Result;
+                else
+                {
+                    //                    client.DefaultRequestHeaders.TransferEncoding.Add(new TransferCodingHeaderValue("chunked"));
+                    //                    client.DefaultRequestHeaders.TransferEncoding.Add(new TransferCodingHeaderValue("gzip"));
+                    result = client.PostAsync(url, content).Result;
+                }
+            }
+
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                txtResponse.Text = result.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                txtResponse.Text = @"Request failed. Status code: " + result.StatusCode + ". Message Content: " + result.Content.ReadAsStringAsync().Result;
+            }
+
+            button1.Text = "Process request to server >>>";
+        }
+
+        private void txtRequest_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control & e.KeyCode == Keys.A)
+            {
+                txtRequest.SelectAll();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cbAction.SelectedItem = "getVersion";
+        }
+
+        private void btnGetCapabilities_Click(object sender, EventArgs e)
+        {
+            cbAction.SelectedItem = "getCapabilities";
+        }
+
+        private void btnInvASA_Click(object sender, EventArgs e)
+        {
+            txtRequest.Text = File.ReadAllText(@"./TestXML/Inventory_ASAHOTEL.xml");
+            cbAction.SelectedItem = "OTA_HotelInvNotif:Inventory";
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            txtRequest.Text = File.ReadAllText(@"./TestXML/GuestRequestsTest.xml");
+            cbAction.SelectedItem = "OTA_Read:GuestRequests";
+        }
+
+
+
+        private void txtResponse_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control & e.KeyCode == Keys.A)
+            {
+                txtResponse.SelectAll();
+            }
+
+        }
+
+        private void ValidateXSD(string sXml, string XsdFilePath)
+        {
+            // string.Empty;
+            try
+            {
+                XmlDocument document = new XmlDocument();
+                document.LoadXml(sXml);
+                sXSDValidationErrorString = "";
+
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add("http://www.opentravel.org/OTA/2003/05", XsdFilePath);
+                settings.Schemas.Compile();
+                settings.ValidationEventHandler += new ValidationEventHandler(ValidationXSDCallBack);
+                settings.ValidationType = ValidationType.Schema;
+
+                XmlReader vreader = XmlReader.Create(new StringReader(document.OuterXml), settings);
+                while (vreader.Read()) { }
+                vreader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                this.sXSDValidationErrorString = ex.Message;
+            }
+        }
+
+        private void ValidationXSDCallBack(object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+                this.sXSDValidationErrorString += "Warning: Matching schema not found. No validation occurred (" + args.Message + ").";
+            else
+                this.sXSDValidationErrorString += "Validation error: " + args.Message;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.ValidateXSD(txtRequest.Text, "./xsd/alpinebits2014-04.xsd");
+            if (sXSDValidationErrorString.Length > 0)
+                MessageBox.Show(sXSDValidationErrorString);
+            else MessageBox.Show("Success!");
+        }
+
+
+        public string getAppSetting(string key)
+        {
+            //Laden der AppSettings
+            Configuration config = ConfigurationManager.OpenExeConfiguration(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            //Zurückgeben der dem Key zugehörigen Value
+            if (config.AppSettings.Settings[key] != null)
+                return config.AppSettings.Settings[key].Value;
+            else return "";
+        }
+
+        public void setAppSetting(string key, string value)
+        {
+            //Laden der AppSettings
+            Configuration config = ConfigurationManager.
+                                    OpenExeConfiguration(
+                                    System.Reflection.Assembly.
+                                    GetExecutingAssembly().Location);
+            //Überprüfen ob Key existiert
+            if (config.AppSettings.Settings[key] != null)
+            {
+                //Key existiert. Löschen des Keys zum "überschreiben"
+                config.AppSettings.Settings.Remove(key);
+            }
+            //Anlegen eines neuen KeyValue-Paars
+            config.AppSettings.Settings.Add(key, value);
+            //Speichern der aktualisierten AppSettings
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            setAppSetting("Username", txtUsername.Text);
+            setAppSetting("Password", txtPassword.Text);
+            setAppSetting("ClientID", txtMessagePWD.Text);
+            setAppSetting("ProtocolVersion", txtCompanyName.Text);
+            setAppSetting("Action", cbAction.Text);
+            setAppSetting("Request", txtRequest.Text);
+            setAppSetting("ServerUrl", txtServer.Text);
+            setAppSetting("BGZIPReturn", cBGZIP.Checked ? "yes" : "no");
+            setAppSetting("BGZIPSend", cBGZIPSend.Checked ? "yes" : "no");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtUsername.Text = getAppSetting("Username");
+            txtPassword.Text =  getAppSetting("Password");
+            txtMessagePWD.Text =  getAppSetting("ClientID");
+            txtCompanyName.Text = getAppSetting("ProtocolVersion");
+            cbAction.Text = getAppSetting("Action");
+            txtServer.Text = getAppSetting("ServerUrl");
+            txtRequest.Text = getAppSetting("Request");
+            cBGZIP.Checked = getAppSetting("BGZIPReturn")=="yes" ? true : false;
+            cBGZIPSend.Checked = getAppSetting("BGZIPSend") == "yes" ? true : false;
+        }
+
+
+        // XML to string
+        private string xmlToString(XmlDocument doc)
+        {
+            XmlDocument xd = doc;
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+            XmlTextWriter xtw = null;
+            try
+            {
+                xtw = new XmlTextWriter(sw);
+                xtw.Formatting = System.Xml.Formatting.Indented;
+                xd.WriteTo(xtw);
+            }
+            finally
+            {
+                if (xtw != null)
+                    xtw.Close();
+            }
+
+            return sb.ToString();
+        }
+
+
+        //Highlights the XML in the richTextBox
+        private void highlightText(RichTextBox rtb)
+        {
+            int Position = rtb.SelectionStart;
+            int k = 0;
+
+            string str = rtb.Text;
+
+            int st, en;
+            int lasten = -1;
+            while (k < str.Length)
+            {
+                st = str.IndexOf('<', k);
+
+                if (st < 0)
+                    break;
+
+                if (lasten > 0)
+                {
+                    rtb.Select(lasten + 1, st - lasten - 1);
+                    rtb.SelectionColor = Color.Black;
+                }
+
+                en = str.IndexOf('>', st + 1);
+                if (en < 0)
+                    break;
+
+                k = en + 1;
+                lasten = en;
+
+                if (str[st + 1] == '!')
+                {
+                    rtb.Select(st + 1, en - st - 1);
+                    rtb.SelectionColor = Color.Green;
+                    continue;
+
+                }
+                String nodeText = str.Substring(st + 1, en - st - 1);
+
+                bool inString = false;
+
+                int lastSt = -1;
+                int state = 0;
+                /* 0 = before node name
+                 * 1 = in node name
+                   2 = after node name
+                   3 = in attribute
+                   4 = in string
+                   */
+                int startNodeName = 0, startAtt = 0;
+                for (int i = 0; i < nodeText.Length; ++i)
+                {
+                    if (nodeText[i] == '"')
+                        inString = !inString;
+
+                    if (inString && nodeText[i] == '"')
+                        lastSt = i;
+                    else
+                        if (nodeText[i] == '"')
+                        {
+                            rtb.Select(lastSt + st + 2, i - lastSt - 1);
+                            rtb.SelectionColor = Color.Blue;
+                        }
+
+                    switch (state)
+                    {
+                        case 0:
+                            if (!Char.IsWhiteSpace(nodeText, i))
+                            {
+                                startNodeName = i;
+                                state = 1;
+                            }
+                            break;
+                        case 1:
+                            if (Char.IsWhiteSpace(nodeText, i))
+                            {
+                                rtb.Select(startNodeName + st, i - startNodeName + 1);
+                                rtb.SelectionColor = Color.Firebrick;
+                                state = 2;
+                            }
+                            break;
+                        case 2:
+                            if (!Char.IsWhiteSpace(nodeText, i))
+                            {
+                                startAtt = i;
+                                state = 3;
+                            }
+                            break;
+
+                        case 3:
+                            if (Char.IsWhiteSpace(nodeText, i) || nodeText[i] == '=')
+                            {
+                                rtb.Select(startAtt + st, i - startAtt + 1);
+                                rtb.SelectionColor = Color.Red;
+                                state = 4;
+                            }
+                            break;
+                        case 4:
+                            if (nodeText[i] == '"' && !inString)
+                                state = 2;
+                            break;
+                    }
+                }
+                if (state == 1)
+                {
+                    rtb.Select(st + 1, nodeText.Length);
+                    rtb.SelectionColor = Color.Firebrick;
+                }
+            }
+            rtb.SelectionStart = Position;
+            rtb.SelectionLength = 0;
+        }
+
+        private void txtRequest_TextChanged(object sender, EventArgs e)
+        {
+            highlightText(txtRequest);
+        }
+
+        private void txtResponse_TextChanged(object sender, EventArgs e)
+        {
+            highlightText(txtResponse);
+        }
+
+    }
+}
